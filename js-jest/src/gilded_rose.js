3f4ae10877e3f2c03f923e925f4ca30e56091abb
hanc.han <chihchun.han@gmail.com>
2022-03-04 11:23:17 +0800

    整併兩個 if / else 區塊

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -27,6 +27,10 @@ class Shop {
       }
       if (item.name == "Aged Brie") {
         this.increaseQuality(item);
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+          this.increaseQuality(item);
+        }
       } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
         this.increaseQuality(item);
         if (item.sellIn < 11) {
@@ -35,18 +39,14 @@ class Shop {
         if (item.sellIn < 6) {
           this.increaseQuality(item);
         }
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+          item.quality = item.quality - item.quality;
+        }
       } else {
         this.decreaseQuality(item);
-      }
-
-      item.sellIn = item.sellIn - 1;
-
-      if (item.sellIn < 0) {
-        if (item.name == "Aged Brie") {
-          this.increaseQuality(item);
-        } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-          item.quality = item.quality - item.quality;
-        } else {
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
           this.decreaseQuality(item);
         }
       }
