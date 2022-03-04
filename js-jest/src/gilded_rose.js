b7325d725a3ca00ac36e190755134e568267816c
hanc.han <chihchun.han@gmail.com>
2022-03-04 11:17:18 +0800

    把反向邏輯改成正向

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -22,13 +22,13 @@ class Shop {
   }
   updateQuality() {
     this.items.forEach((item) => {
+      if (item.name == "Sulfuras, Hand of Ragnaros") {
+        return;
+      }
       if (
-        item.name != "Aged Brie" &&
-        item.name != "Backstage passes to a TAFKAL80ETC concert" &&
-        item.name != "Sulfuras, Hand of Ragnaros"
+        item.name == "Aged Brie" ||
+        item.name == "Backstage passes to a TAFKAL80ETC concert" 
       ) {
-        this.decreaseQuality(item);
-      } else {
         this.increaseQuality(item);
         if (
           item.name == "Backstage passes to a TAFKAL80ETC concert" &&
@@ -42,21 +42,19 @@ class Shop {
         ) {
           this.increaseQuality(item);
         }
+      } else {
+        this.decreaseQuality(item);
       }
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
-        item.sellIn = item.sellIn - 1;
-      }
+
+      item.sellIn = item.sellIn - 1;
+
       if (item.sellIn < 0) {
-        if (item.name != "Aged Brie") {
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-              this.decreaseQuality(item);
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
+        if (item.name == "Aged Brie") {
           this.increaseQuality(item);
+        } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+          item.quality = item.quality - item.quality;
+        } else {
+          this.decreaseQuality(item);
         }
       }
     });
