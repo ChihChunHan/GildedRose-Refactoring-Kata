bfdee5e7e342972f6a5618c52c002b9a72fee0cb
hanc.han <chihchun.han@gmail.com>
2022-03-04 13:49:37 +0800

    抽出函式

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -17,41 +17,46 @@ class Item {
   decreaseSellIn() {
     this.sellIn = this.sellIn - 1;
   }
+  updateDefaultItemQuality() {
+    this.decreaseQuality();
+    if (this.sellIn < 1) {
+      this.decreaseQuality();
+    }
+  }
+  updateBackstagePassQuality() {
+    this.increaseQuality();
+    if (this.sellIn < 11) {
+      this.increaseQuality();
+    }
+    if (this.sellIn < 6) {
+      this.increaseQuality();
+    }
+    if (this.sellIn < 1) {
+      this.quality = 0;
+    }
+  }
+  updateBrieQuality() {
+    this.increaseQuality();
+    if (this.sellIn < 1) {
+      this.increaseQuality();
+    }
+  }
   updateItemQuality() {
     switch (this.name) {
       case "Sulfuras, Hand of Ragnaros":
         break;
       case "Aged Brie":
-        this.increaseQuality();
-        if (this.sellIn < 1) {
-          this.increaseQuality();
-        }
+        this.updateBrieQuality();
         break;
       case "Backstage passes to a TAFKAL80ETC concert":
-        this.increaseQuality();
-        if (this.sellIn < 11) {
-          this.increaseQuality();
-        }
-        if (this.sellIn < 6) {
-          this.increaseQuality();
-        }
-        if (this.sellIn < 1) {
-          this.quality = 0;
-        }
+        this.updateBackstagePassQuality();
         break;
       case "Conjured Mana Cake":
-        this.decreaseQuality();
-        this.decreaseQuality();
-        if (this.sellIn < 1) {
-          this.decreaseQuality();
-          this.decreaseQuality();
-        }
+        this.updateDefaultItemQuality();
+        this.updateDefaultItemQuality();
         break;
       default:
-        this.decreaseQuality();
-        if (this.sellIn < 1) {
-          this.decreaseQuality();
-        }
+        this.updateDefaultItemQuality();
         break;
     }
     this.decreaseSellIn();
